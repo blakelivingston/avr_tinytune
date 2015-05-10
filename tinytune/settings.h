3facf5566d1b4721c82c9165abbc9e928ff030c2
Blake Livingston <blake.a.livingston@gmail.com>
2015-05-09 21:38:57 -0700

    work

--- tinytune/settings.h
+++ tinytune/settings.h
@@ -1,9 +1,9 @@
 // CPU clock rate
 #define F_CPU 8000000
 // Number of bytes to pre-calculate. Not conclusively shown that this helps much.
-#define SAMPLE_BUFFER 32
+#define SAMPLE_BUFFER 20
 // Sample clock updates at this rate in Hz
-#define SAMPLE_RATE 44000UL
+#define SAMPLE_RATE 20000UL
 // Max number of voices
 #define N_VOICES 5
 // Right shift divide for output waveform. Use for course global volume setting
